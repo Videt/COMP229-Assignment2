7f2c333a89a6a4e531de65695eecea6209281880
Videt <egor310501@mail.ru>
2020-10-09 17:05:23 +1100

    added comments to app.js

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+let logger = require('morgan'); // logs requests and responses
 
+// do all routing in these files
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 
@@ -16,12 +17,12 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs'); // express -e
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev')); // logs the information about requests
+app.use(express.json()); // helps to recognize JSON objects
+app.use(express.urlencoded({ extended: false })); // allows to read URL data (GET requests)
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'public'))); // use public directory
+app.use(express.static(path.join(__dirname, 'node_modules'))); // use node_modules directory
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
